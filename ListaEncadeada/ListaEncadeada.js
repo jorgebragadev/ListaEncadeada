ea70311db4e500ba4693970c25da7565b9a096a4
Jorge Braga <117918908+jorgebragadev@users.noreply.github.com>
2023-09-12 11:07:58 -0300

    Update ListaEncadeada.js

--- ListaEncadeada/ListaEncadeada.js
+++ ListaEncadeada/ListaEncadeada.js
@@ -1,20 +1,74 @@
-
 class Pessoa {
   constructor(nome, idade) {
     this.nome = nome;
     this.idade = idade;
-    this.filho = null; 
+    this.filhos = null;
+    this.proximo = null;
   }
 }
 
-const pessoa1 = new Pessoa("Alice", 30);
-const pessoa2 = new Pessoa("Bob", 35);
-const pessoa3 = new Pessoa("Carol", 25);
-const pessoa4 = new Pessoa("David", 28);
+class ListaEncadeada {
+  constructor() {
+    this.cabeca = null;
+  }
 
+  adicionarPessoa(nome, idade) {
+    const novaPessoa = new Pessoa(nome, idade);
 
-pessoa1.filho = pessoa2;
-pessoa2.filho = pessoa3;
-pessoa3.filho = pessoa4;
+    if (!this.cabeca) {
+      this.cabeca = novaPessoa;
+    } else {
+      let atual = this.cabeca;
+      while (atual.proximo) {
+        atual = atual.proximo;
+      }
+      atual.proximo = novaPessoa;
+    }
+  }
 
+  adicionarFilho(nomePessoa, nomeFilho, idadeFilho) {
+    let atual = this.cabeca;
+    while (atual) {
+      if (atual.nome === nomePessoa) {
+        if (!atual.filhos) {
+          atual.filhos = new Pessoa(nomeFilho, idadeFilho);
+        } else {
+          let filhoAtual = atual.filhos;
+          while (filhoAtual.proximo) {
+            filhoAtual = filhoAtual.proximo;
+          }
+          filhoAtual.proximo = new Pessoa(nomeFilho, idadeFilho);
+        }
+        return;
+      }
+      atual = atual.proximo;
+    }
+    console.log(`Pessoa com nome ${nomePessoa} não encontrada.`);
+  }
 
+  imprimirLista() {
+    let atual = this.cabeca;
+    while (atual) {
+      console.log(`Nome: ${atual.nome}, Idade: ${atual.idade}`);
+      if (atual.filhos) {
+        let filhoAtual = atual.filhos;
+        while (filhoAtual) {
+          console.log(`- Filho: ${filhoAtual.nome}, Idade: ${filhoAtual.idade}`);
+          filhoAtual = filhoAtual.proximo;
+        }
+      }
+      atual = atual.proximo;
+    }
+  }
+}
+
+// Exemplo de uso
+const lista = new ListaEncadeada();
+
+lista.adicionarPessoa("João", 30);
+lista.adicionarPessoa("Maria", 28);
+lista.adicionarFilho("João", "Pedro", 5);
+lista.adicionarFilho("João", "Ana", 3);
+lista.adicionarFilho("Maria", "Lucas", 2);
+
+lista.imprimirLista();
